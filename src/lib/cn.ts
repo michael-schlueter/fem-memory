a5ffa21d669d4dd10b11bac38b0ca32d0a80379d
rulo <schluet0r4life@gmail.com>
2026-08-12 19:45:33 +0200

    fix(lib): Prevent text-preset-* from colliding with text-color in cn()

--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -1,4 +1,14 @@
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": [
+        { text: ["preset-1", "preset-2", "preset-3", "preset-4", "preset-5", "preset-6", "preset-7", "preset-8", "preset-9", "preset-10", "preset-11", "preset-12", "preset-13", ]}
+      ],
+    },
+  }
+})
 
 export function cn(...classes: Array<string | false | null | undefined>) {
   return twMerge(classes.filter(Boolean).join(" "));
