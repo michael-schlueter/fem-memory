f3121e981960b90ab1215db34c6731c8b107d1c9
rulo <schluet0r4life@gmail.com>
2026-08-12 18:01:58 +0200

    fix(lib): install correct tailwind-merge package

--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -1,4 +1,4 @@
-import { twMerge } from "tw-merge";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...classes: Array<string | false | null | undefined>) {
   return twMerge(classes.filter(Boolean).join(" "));
