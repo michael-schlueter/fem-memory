6003842454009345a4e45e31fec653411fd4f9c5
rulo <schluet0r4life@gmail.com>
2026-08-12 14:11:38 +0200

    feat(screens): scaffold Start/Game screens and wire up switching in App

---
+++ src/screens/GameScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const GameScreen = () => {
+  return (
+    <div>GameScreen</div>
+  )
+}
---
+++ src/screens/StartScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const StartScreen = () => {
+  return (
+    <div>StartScreen</div>
+  )
+}
